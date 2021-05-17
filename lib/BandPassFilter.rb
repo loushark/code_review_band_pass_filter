25e5410a35902eb7157f44dda3f5a234576bcfd9
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-05-17 11:25:52 +0100

    second test passed red green

--- lib/BandPassFilter.rb
+++ lib/BandPassFilter.rb
@@ -1,5 +1,12 @@
 class BandPassFilter
-  def filter(input, min_freq, max_freq)
-    input
+
+  def filter(sound_wave, min_freq, max_freq)
+    new_sound_wave = []
+
+    if sound_wave[0] < min_freq
+      new_sound_wave << min_freq
+    else
+      sound_wave
+    end
   end
 end
--- spec/BandPassFilter_spec.rb
+++ spec/BandPassFilter_spec.rb
@@ -8,5 +8,10 @@ describe BandPassFilter do
       input = [10]
       expect(test_filter.filter(input, 10, 30)).to eq [10]
     end
+
+    it 'returns a changed sound wave when it does not meet the min frequency requirements' do
+      input = [10]
+      expect(test_filter.filter(input, 20, 30)).to eq [20]
+    end
   end
 end
