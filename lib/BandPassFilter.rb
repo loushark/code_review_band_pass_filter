05f85712381d6068513c68959c0fd39c52b6bde8
Lou Reade <39278690+loushark@users.noreply.github.com>
2021-08-10 20:36:55 +0100

    ternary statement more readable

--- lib/BandPassFilter.rb
+++ lib/BandPassFilter.rb
@@ -13,7 +13,13 @@ private
 
   def change_sound_wave(sound_wave, min_freq, max_freq)
     new_sound_wave = sound_wave.map do |freq|
-      freq < min_freq ? min_freq : freq && freq > max_freq ? max_freq : freq
+      if freq < min_freq
+        min_freq
+      elsif freq > max_freq
+        max_freq
+      else 
+        freq
+      end
     end
   end
 end
